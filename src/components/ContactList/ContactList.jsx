defb571999435098f34af4c31094d5d39c774208
leguter <kolalevcenko80@gmail.com>
2024-09-02 22:44:38 -1200

    problem with opmization

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,21 +2,21 @@ import Contact from "../Contact/Contact";
 import css from './ContactList.module.css'
 import { deleteContact } from "../../redux/contactsOps";
 import { useDispatch, useSelector } from "react-redux";
-import { selectContacts } from "../../redux/contactsSlice";
+// import { selectContacts } from "../../redux/contactsSlice";
+import { filteredContacts } from "../../redux/selectors";
 const ContactList = () => {
     const dispatch = useDispatch();
-    const filterValue = useSelector((state) => state.filter.filterValue);
-const contacts = useSelector(selectContacts);
+// const contacts = useSelector(selectContacts);
 function deleteCard(item) {
     const action = deleteContact(item.id);
     dispatch(action);
-}
-       const filteredCards = contacts.filter((card) =>
-         card.name.toLowerCase().includes(filterValue.toLowerCase())
-       )
+} const filtedContacts = useSelector(filteredContacts )
+      //  const filteredCards = contacts.filter((card) =>
+      //    card.name.toLowerCase().includes(filterValue.toLowerCase())
+      //  )
        return (
          <ul className={css.list}>
-               {filteredCards.map((card) => (
+               {filtedContacts.map((card) => (
              <li key={card.id}>
                <Contact
                  number={card.number}
